2ef7dcb735becc6c4aa5de09b57c7b906cb7e532
carlosdaniel05 <91232207+carlosdaniel05@users.noreply.github.com>
2021-10-14 18:23:03 -0500

    Update homework.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -3,7 +3,7 @@
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+  //Tu código: xd
 }
 
 function invocarCallback(cb) {
